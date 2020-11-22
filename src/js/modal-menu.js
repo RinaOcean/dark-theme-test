21550e3829f4bff353aff42be1cdd65127d58050
RinaOcean <antyfifa@gmail.com>
2020-11-22 11:26:52 +0200

    test-theme

---
+++ src/js/modal-menu.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      
+      modal: document.querySelector('[data-modal]'),
+    };
+
+    refs.openModalBtn.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+      document.body.classList.toggle("modal-open");
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
